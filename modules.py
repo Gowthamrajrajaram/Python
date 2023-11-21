d9f9519bee23f109b669bc3b66bbe430058f3338
Gowthamrajrajaram <123543651+Gowthamrajrajaram@users.noreply.github.com>
2023-11-21 10:52:43 +0530

    modules

---
+++ modules.py
@@ -0,0 +1,30 @@
+import calculator
+
+print(calculator.add(10,90))
+print(calculator.sub(50,40))
+print(calculator.multi(5,6))
+print(calculator.div(10,2))
+
+
+# from calculator import add
+# print(add(10,56))
+
+
+# from calculator import * # * means all members present in the  calculator module
+# print(add(5,9))
+# print(div(15,3))
+
+
+# import calculator as c
+# print(c.add(10,90))
+# print(c.sub(50,40))
+# print(c.multi(5,6))
+# print(c.div(10,2))
+
+# from calculator import add as a ,sub as b,multi as c
+# print(a(10,90))
+# print(b(50,40))
+# print(c(5,6))
+
+# from calculator import add as addition
+# print(addition(1,2))
